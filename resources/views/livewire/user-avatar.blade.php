dd8f444b59ec1eb6e1c7af21ca16ef0421f82720
mason.hows11 <mason.hows11@gmail.com>
2022-11-19 21:48:07 +0330

    add style for image upload user profile section

--- resources/views/livewire/user-avatar.blade.php
+++ resources/views/livewire/user-avatar.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="row user-avatar d-flex flex-column">
 
-        <div class="user-img d-flex justify-content-center">
+        <div class="user-img mt-2 d-flex justify-content-center">
             <img src="{{ $user->image_path ?  asset('storage/users/'.$user->image_path)  : asset('images/users/no-image-icon-23494.png') }}"
                  class="rounded avatar-previewer" alt="user-avatar">
         </div>
