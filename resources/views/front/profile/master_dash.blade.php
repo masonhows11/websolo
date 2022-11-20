97e66df90c4cb3ecd70018cf602a7d73de45e7b9
mason.hows11 <mason.hows11@gmail.com>
2022-11-20 18:44:02 +0330

    profile img preview updated

--- resources/views/front/profile/master_dash.blade.php
+++ resources/views/front/profile/master_dash.blade.php
@@ -1,12 +1,14 @@
 @extends('front.include.master_auth')
 @section('main_content')
     <div class="container">
-        <div class="row row-cols-xxl-2 row-cols-xl-2  row-cols-lg-1 row-cols-md-1  row-cols-1 user-dashboard-section">
+        <div class="row row-cols-xxl-2 row-cols-xl-2   row-cols-1 user-dashboard-section">
 
             <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-xs-4 col dash_menu_right_side">
+
                 <div class="col d-flex justify-content-center user-image-profile rounded-2">
                     <livewire:user-avatar/>
                 </div>
+
                 <div class="col user-profile-menu mt-2 rounded-2">
                     <ul class="mx-auto my-4">
                         <li class=""><i class="fa fa-home-user me-3"></i><a href="{{ route('dashboard') }}">اطلاعات کاربری</a></li>
@@ -18,6 +20,7 @@
                         <li class=""><i class="fa fa-sign-out me-3"></i><a href="{{ route('logout') }}">خروج</a></li>
                     </ul>
                 </div>
+
             </div>
 
             <div class="col-xxl-8 col-xl-8 col-lg-8 col-md-8 col-xs-8 col info_dash_left_side  rounded-2 mt-1 ">
--- resources/views/livewire/user-avatar.blade.php
+++ resources/views/livewire/user-avatar.blade.php
@@ -1,22 +1,23 @@
 <div>
     <div class="row user-avatar d-flex flex-column">
 
-        <div class="user-img mt-2 d-flex justify-content-center">
-            <img
-                src="{{ $user->image_path ?  asset('storage/users/'.$user->image_path)  : asset('images/users/no-image-icon-23494.png') }}"
-                class="rounded avatar-previewer" alt="user-avatar">
-        </div>
-
-        <div class="user-name">
-            <p class="text-center">{{ $user->name }}</p>
-            <p class="text-center">{{ $user->first_name }} {{ $user->last_name }} </p>
-        </div>
-
-
-        <div class="d-flex  flex-column my-2 userAvatarFile">
         <form wire:submit.prevent="save">
+            <div class="user-img mt-2 d-flex justify-content-center">
+                @if($avatar)
+                    <img src="{{ $avatar->temporaryUrl() }}" alt="">
+                    @else
+                    <img src="{{ $user->image_path ?  asset('storage/users/'.$user->image_path)  : asset('images/users/no-image-icon-23494.png') }}"
+                         class="rounded avatar-previewer" alt="user-avatar">
+                @endif
+            </div>
+
+            <div class="user-name">
+                <p class="text-center">{{ $user->name }}</p>
+                <p class="text-center">{{ $user->first_name }} {{ $user->last_name }} </p>
+            </div>
 
 
+            <div class="d-flex  flex-column my-2 userAvatarFile">
                 <div class="my-3 text-center mx-auto">
                     <label for="avatarFile" class="text-center">
                         انتخاب عکس
@@ -33,8 +34,7 @@
                     {{ $message }}
                 </span>
                 @enderror
-
+            </div>
         </form>
-        </div>
     </div>
 </div>
